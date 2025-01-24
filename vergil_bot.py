54b1ef8a76969c9b7eb1fe7f22dbe9782c82872b
juicy <wopjr1234@gmail.com>
2025-01-23 21:39:12 -0500

    Added two people

--- vergil_bot.py
+++ vergil_bot.py
@@ -28,6 +28,18 @@ async def on_voice_state_update(member, before, after):
             await asyncio.sleep(180)
             if await checkuser(member):
                 await member.send('You are muted')
+    if member.id == names.jacobID:
+        if after.self_mute:
+            print('Jacob is muted')
+            await asyncio.sleep(180)
+            if await checkuser(member):
+                await member.send('You are muted')
+    if member.id == names.devinID:
+        if after.self_mute:
+            print('Devin is muted')
+            await asyncio.sleep(180)
+            if await checkuser(member):
+                await member.send('You are muted')
 
     if not before.channel and after.channel and member.id == names.reggieID and before.self_mute:
         print('REGGIE HOW DO YOU JOIN MUTED EVERYTIME')
